3000d049dcf1392820025929694c76357b8fff4b
montychen <dj2503@qq.com>
2023-03-29 22:53:28 +0800

    在fastapi_db目录下创建sqlite数据库文件

--- databases.py
+++ databases.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base  # type: ignore
 from sqlalchemy.orm import sessionmaker  # type: ignore
 
 # mysql连接字符串是："mysql+pymysql://用户名:密码@主机IP:端口/数据库名?charset=utf8"
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi_db/sqlite_app.db"
 engine = create_engine(
     # sqlite3数据库本身并非一个网络数据库，其默认只能在同一线程中使用，如果不设定该标记，则SQLAlchemy会提示错误
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
